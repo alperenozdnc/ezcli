f4d2bf09e4571947384d62e3cb0a00f6410774ae
alperenozdnc <alperen@ozdinc.org>
2025-11-20 01:32:07 +0300

    refactor: move internals to `src`

--- include/ezcli/internal.h
+++
@@ -1,6 +0,0 @@
-#ifndef EZCLI_INTERNAL_H
-#define EZCLI_INTERNAL_H
-
-#include <ezcli/expand.h>
-
-#endif
--- src/ezcli/internal/expand.c
+++ src/ezcli/internal/expand.c
@@ -1,4 +1,4 @@
-#include <ezcli/expand.h>
+#include "expand.h"
 
 #include <stdlib.h>
 #include <string.h>
