1e72d5ddf6f141b19295266ee9728d2617c8f470
alperenozdnc <alperen@ozdinc.org>
2025-11-20 01:44:02 +0300

    refactor: `ezcli.h` is now an umbrella

--- include/ezcli.h
+++ include/ezcli.h
@@ -4,21 +4,10 @@
 #include <ezcli/cli.h>
 #include <ezcli/opt.h>
 
+#include <ezcli/addopt.h>
+#include <ezcli/freecli.h>
 #include <ezcli/initcli.h>
 #include <ezcli/print.h>
 #include <ezcli/runcli.h>
-
-#include <stdbool.h>
-
-/*
- * adds an option to the `opts` dynamic array in a `cli` struct.
- */
-void cliadd_opt(struct cli *cli_p, struct opt *opt_a);
-
-/*
- * frees all allocated memory. this is the only function you need to
- * run for cleanup.
- */
-void freecli(struct cli *cli_p);
 
 #endif
