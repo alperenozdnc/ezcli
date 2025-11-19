4779b006f891f4d704225a996078e4c87220e80b
alperenozdnc <alperen@ozdinc.org>
2025-11-19 17:35:51 +0300

    chore: make `runcli` print args as placeholder

--- src/ezcli/ezcli.c
+++ src/ezcli/ezcli.c
@@ -1,12 +1,24 @@
 #include <ezcli.h>
 
 #include <stdbool.h>
+#include <stdio.h>
 
 void initcli(struct cli *cli, char *cmd, char **aliases, bool gen_help,
              struct opt **opts) {
 }
 
 void runcli(struct cli *cli, int argc, char *argv[]) {
+    printf("argc=%d, ", argc);
+
+    for (int i = 0; i < argc; i++) {
+        printf("%s", argv[i]);
+
+        if (i != argc - 1) {
+            printf(" ");
+        }
+    }
+
+    printf("\n");
 }
 
 void cliadd_opt(struct cli *cli_p, struct opt opt_a) {
