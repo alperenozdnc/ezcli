edd18572df4e6cfd043e654aad0abeaeefee06e6
alperenozdnc <alperen@ozdinc.org>
2025-11-19 19:21:12 +0300

    chore: fix unsynced type

--- src/ezcli/ezcli.c
+++ src/ezcli/ezcli.c
@@ -21,7 +21,7 @@ void runcli(struct cli *cli, int argc, char *argv[]) {
     printf("\n");
 }
 
-void cliadd_opt(struct cli *cli_p, struct opt opt_a) {
+void cliadd_opt(struct cli *cli_p, struct opt *opt_a) {
 }
 
 void cliadd_alias(struct cli *cli_p, char *alias) {
