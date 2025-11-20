22cb186d5f1d1a1ac2aa330d3ef5a6bedbdfb4f6
alperenozdnc <alperen@ozdinc.org>
2025-11-20 21:10:44 +0300

    refactor: `initcli()` allocates memory now this is easier for freeing later.

--- src/ezcli/initcli.c
+++ src/ezcli/initcli.c
@@ -1,13 +1,27 @@
 #include <ezcli/cli.h>
 #include <ezcli/initcli.h>
 
+#include "internal/opts_size.h"
+
+#include <stdlib.h>
+#include <string.h>
+
 void initcli(struct cli *cli, char *cmd, bool gen_help, struct opt **opts) {
     cli->cmd = cmd;
     cli->gen_help = gen_help;
-    cli->opts = opts;
     cli->opts_len = 0;
 
-    while (cli->opts[cli->opts_len] != NULL) {
+    while (opts[cli->opts_len] != NULL) {
         cli->opts_len++;
     }
+
+    cli->opts = malloc(opts_size(cli->opts_len));
+
+    for (size_t i = 0; i < cli->opts_len; i++) {
+        struct opt *opt = opts[i];
+
+        memcpy(&cli->opts[i], &opt, sizeof(struct opt *));
+    }
+
+    cli->opts[cli->opts_len] = NULL;
 }
