d131351e45f5c10f64798331cb32c1be0bbb6efb
alperenozdnc <alperen@ozdinc.org>
2025-11-23 16:26:20 +0300

    chore: add `desc` field to `struct cli`

--- include/ezcli/cli.h
+++ include/ezcli/cli.h
@@ -10,8 +10,9 @@
  * keeps the information about the general command line interface.
  * `cmd`: name of the command. this is used for parsing and generating help
  * options.
- * `allow_non_opt`: if `true` it allows non for option-preceded arguments, like
- * `ls /home` where
+ * `desc`: description of the command. this is used for generating help options.
+ * `allow_non_opt`: if `true` it allows non for option-preceded
+ * arguments, like `ls /home` where
  * `/home` is the non option-preceded argument. you should create an opt in
  * `opts` that has name `EZCLI_NONOPT` and type `OPTION_BARE` to handle these
  * inputs. `opts`: list of options the command line interface supports.
@@ -20,6 +21,7 @@
  */
 struct cli {
     char *cmd;
+    char *desc;
     bool allow_non_opt;
     struct opt **opts;
     size_t opts_len;
--- src/ezcli/initcli.c
+++ src/ezcli/initcli.c
@@ -8,9 +8,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-void initcli(struct cli *cli, char *cmd, bool allow_non_opt, struct opt **opts,
-             char **help_aliases) {
+void initcli(struct cli *cli, char *cmd, char *desc, bool allow_non_opt,
+             struct opt **opts, char **help_aliases) {
     cli->cmd = cmd;
+    cli->desc = desc;
     cli->allow_non_opt = allow_non_opt;
     cli->opts_len = 0;
 
