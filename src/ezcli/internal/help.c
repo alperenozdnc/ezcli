fb255a30b41738d2ddfa0b4a199bc2b00563097d
alperenozdnc <alperen@ozdinc.org>
2025-11-23 18:03:33 +0300

    refactor: isolate different printing concerns and improve readability also, some coloring.

--- src/ezcli/internal/help.c
+++ src/ezcli/internal/help.c
@@ -1,5 +1,6 @@
 #include <ezcli/cli.h>
 #include <ezcli/opt.h>
+#include <ezcli/print.h>
 
 #include "expand.h"
 #include "help.h"
@@ -7,45 +8,57 @@
 #include <stdio.h>
 #include <string.h>
 
-void cli_help(struct cli *cli, struct opt **opts) {
-    printf("[USAGE]: %s %s\n", cli->cmd, cli->usage);
-    printf("%s\n\n", cli->desc);
+#define PADDING_LEFT "    " // 4 spaces
 
+void __print_header(struct cli *cli) {
+    cliprint(CLI_HINT, EZCLI_EMPTY_PREFIX, "[USAGE]: %s %s", cli->cmd,
+             cli->usage);
+
+    printf("%s\n\n", cli->desc);
+}
+
+void __print_options(struct cli *cli, struct opt **opts) {
     int i = 0;
 
-    printf("    ");
-
     while (cli->help_aliases[i]) {
-        printf("%s", cli->help_aliases[i]);
+        if (i == 0)
+            printf(PADDING_LEFT);
+
+        printf(ANSI_BLUE "%s" ANSI_RESET, cli->help_aliases[i]);
 
         if (cli->help_aliases[i + 1]) {
             printf(", ");
+        } else {
+            printf(" -> prints this menu.\n");
         }
 
         i++;
     }
 
-    printf(" -> prints this menu.\n");
-
     for (size_t i = 0; i < cli->opts_len; i++) {
         struct opt *opt = opts[i];
 
-        if (strcmp(opt->name, EZCLI_NONOPT) == 0) {
+        if (strcmp(opt->name, EZCLI_NONOPT) == 0)
             continue;
-        }
 
-        printf("    ");
-        printf("%s -> %s", expand(opt), opt->desc);
+        printf(PADDING_LEFT ANSI_BLUE "%s" ANSI_RESET " -> %s", expand(opt),
+               opt->desc);
 
-        if (i != cli->opts_len - 1) {
+        if (i != cli->opts_len - 1)
             printf("\n");
-        }
     }
+}
 
-    printf("\n");
+void __print_footer(struct cli *cli) {
+    if (!cli->footer)
+        return;
 
-    if (cli->footer) {
-        printf("\n");
-        printf("%s", cli->footer);
-    }
+    printf("\n\n");
+    printf("%s", cli->footer);
+}
+
+void cli_help(struct cli *cli, struct opt **opts) {
+    __print_header(cli);
+    __print_options(cli, opts);
+    __print_footer(cli);
 }
