1deb93b737ab43a866da3c4351eb06bae2adf64d
alperenozdnc <alperen@ozdinc.org>
2025-11-27 03:41:15 +0300

    feat: `printalias` fn prints an alias list in blue

---
+++ src/ezcli/internal/printaliases.c
@@ -0,0 +1,29 @@
+#include <ezcli/print.h>
+
+#include "printaliases.h"
+
+#include <stdio.h>
+
+/*
+ * prints at most 3 aliases for an option. color is blue and the separator is
+ * '->'.
+ */
+void printaliases(char **aliases) {
+    int i = 0;
+
+    printf(ANSI_BLUE);
+
+    while (aliases[i] && i <= 2) {
+        char *alias = aliases[i];
+
+        printf("%s", alias);
+
+        if (aliases[i + 1] && i + 1 <= 2) {
+            printf(", ");
+        }
+
+        i++;
+    }
+
+    printf(ANSI_RESET);
+}
