8c89d3f834b8f05b2c3227fc0e9115e3a11ed8e2
alperenozdnc <alperen@ozdinc.org>
2025-11-28 11:53:59 +0300

    feat: `gettok_offset()`

--- include/ezcli/gettok.h
+++ include/ezcli/gettok.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <ezcli/cli.h>
+#include <limits.h>
+
+#define CLI_TOK_FIRST INT_MIN
+#define CLI_TOK_LAST INT_MAX
 
 /*
  * gets the previously parsed token. if idx=1, returns `NULL`.
@@ -11,3 +15,12 @@ char *gettok_prev(cli_s *cli);
  * gets the next parsed token. if idx=end, returns `NULL`.
  */
 char *gettok_next(cli_s *cli);
+
+/*
+ * offsets `n` from the current token's position. returns `NULL` if offset is
+ * too little or too large.
+ *
+ * use `CLI_TOK_FIRST` as `n` for the first token in `argv`, and `CLI_TOK_LAST`
+ * as `n` for the final token.
+ */
+char *gettok_offset(cli_s *cli, int n);
--- src/ezcli/utils/gettok.c
+++ src/ezcli/utils/gettok.c
@@ -1,17 +1,35 @@
 #include <ezcli/cli.h>
 #include <ezcli/gettok.h>
 
+#define ARGV_BEGIN 1
+#define ARGV_END cli->argc - 1
+
 char *gettok_prev(cli_s *cli) {
     // 0th item is the cmd name itself
-    if (cli->tok_idx == 1)
+    if (cli->tok_idx == ARGV_BEGIN)
         return NULL;
 
     return cli->argv[cli->tok_idx - 1];
 }
 
 char *gettok_next(cli_s *cli) {
-    if (cli->tok_idx + 1 == cli->argc)
+    if (cli->tok_idx == ARGV_END)
         return NULL;
 
     return cli->argv[cli->tok_idx + 1];
+}
+
+char *gettok_offset(cli_s *cli, int n) {
+    if (n == CLI_TOK_FIRST) {
+        return cli->argv[ARGV_BEGIN];
+    } else if (n == CLI_TOK_LAST) {
+        return cli->argv[ARGV_END];
+    }
+
+    int k = cli->tok_idx + n;
+
+    if (k >= ARGV_BEGIN && k <= ARGV_END)
+        return cli->argv[k];
+
+    return NULL;
 }
