8b471cb8424786c06b7308ee4fc7caec12a04e34
alperenozdnc <alperen@ozdinc.org>
2025-11-29 01:16:27 +0300

    refactor: skip `delopt()` if option doesn't exist in `opts`

--- src/ezcli/utils/delopt.c
+++ src/ezcli/utils/delopt.c
@@ -1,4 +1,7 @@
+#include <ezcli/cli.h>
 #include <ezcli/delopt.h>
+#include <ezcli/external.h>
+#include <ezcli/print.h>
 
 #include "../internal/check_alloc.h"
 #include "../internal/match.h"
@@ -9,6 +12,15 @@
 #include <string.h>
 
 void delopt(cli_s *cli, opt_s *opt_d) {
+    if (!ot_match_any(cli, opt_d->aliases[0])) {
+        CLI_DEBUG_ONLY(
+            cliprint(CLI_WARN, "[ezcli] ",
+                     "option %s doesn't exist in opts array, skipping...",
+                     opt_d->aliases[0]));
+
+        return;
+    }
+
     size_t new_len = cli->opts_len - 1;
     opt_s **new_opts = malloc(OPTS_SIZE(new_len));
     CLI_CHECK_ALLOC(new_opts);
