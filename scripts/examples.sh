dc5c2e7a1a6e71a60f81ba7f0a49facf715c3b14
alperenozdnc <alperen@ozdinc.org>
2025-11-29 16:03:36 +0300

    feat(scripts): examples aliases script

---
+++ scripts/examples.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+# run source ./scripts/examples.sh from project root
+# to get aliases for the example programs
+
+alias calc="./examples/calc"
+alias docs="./examples/docs"
+alias human="./examples/human"
+alias program="./examples/program"
