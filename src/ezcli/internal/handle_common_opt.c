ac6f6b999ed4e0156a3b79ed97b3fe125ff8004d
alperenozdnc <alperen@ozdinc.org>
2025-12-01 01:24:02 +0300

    style(internal): no braces are better for inliners

--- src/ezcli/internal/handle_common_opt.c
+++ src/ezcli/internal/handle_common_opt.c
@@ -7,9 +7,8 @@
 void handle_common_opt(cli_s *cli, void *ctx, char *tok) {
     opt_s *opt_common = oa_match_first(cli, CLI_COMMON_OPT);
 
-    if (!opt_common) {
+    if (!opt_common)
         return;
-    }
 
     opt_common->body(ctx, tok);
 }
