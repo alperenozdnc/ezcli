6c277fb837bf118a21cf27afdadc10e017f1cd26
alperenozdnc <alperen@ozdinc.org>
2025-11-30 15:28:57 +0300

    feat(internal): common option handling

---
+++ src/ezcli/internal/handle_common_opt.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <ezcli/cli.h>
+
+/*
+ * handles common options. common options are options that are run before
+ * every single successful option body execution.
+ */
+void handle_common_opt(cli_s *cli, void *ctx, char *tok);
