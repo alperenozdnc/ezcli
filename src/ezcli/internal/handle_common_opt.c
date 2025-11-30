b858613c2cda894c30558b280b23e44cff7c347a
alperenozdnc <alperen@ozdinc.org>
2025-12-01 01:29:50 +0300

    style(internal): no need for expression inversion on one-liner

--- src/ezcli/internal/handle_common_opt.c
+++ src/ezcli/internal/handle_common_opt.c
@@ -7,8 +7,6 @@
 void handle_common_opt(cli_s *cli, void *ctx, char *tok) {
     opt_s *opt_common = oa_match_first(cli, CLI_COMMON_OPT);
 
-    if (!opt_common)
-        return;
-
-    opt_common->body(ctx, tok);
+    if (opt_common)
+        opt_common->body(ctx, tok);
 }
