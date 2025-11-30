c7e88dc0ea7ab93207bbc9476a9ef77d82f1ec05
alperenozdnc <alperen@ozdinc.org>
2025-11-30 05:06:59 +0300

    chore(internal): move `panic()` to internal

---
+++ src/ezcli/internal/panic.c
@@ -0,0 +1,13 @@
+#include <ezcli/cli.h>
+#include <ezcli/external.h>
+#include <ezcli/opt.h>
+#include <ezcli/print.h>
+
+ret_e panic() {
+    CLI_DEBUG_ONLY(cliprint(CLI_WARN, "ezcli: ", "panicking"));
+
+    if (CLI_MODE_LAIDBACK)
+        return RET_WARN;
+
+    return RET_FAIL;
+}
---
+++ src/ezcli/internal/panic.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <ezcli/cli.h>
+
+/*
+ * panic with a warning/error based on laidback value.
+ */
+ret_e panic();
