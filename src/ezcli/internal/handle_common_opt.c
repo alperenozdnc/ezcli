3e3a8765776989be820b41b57ecd42ad96fe7ad5
alperenozdnc <alperen@ozdinc.org>
2025-11-30 17:23:38 +0300

    chore(internal): remove dangling printf

--- src/ezcli/internal/handle_common_opt.c
+++ src/ezcli/internal/handle_common_opt.c
@@ -4,13 +4,10 @@
 #include "handle_common_opt.h"
 #include "match.h"
 
-#include <stdio.h>
-
 void handle_common_opt(cli_s *cli, void *ctx, char *tok) {
     opt_s *opt_common = oa_match_first(cli, CLI_COMMON_OPT);
 
     if (!opt_common) {
-        printf("nah ill just ret\n");
         return;
     }
 
