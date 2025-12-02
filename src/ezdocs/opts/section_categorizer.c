4e3ae2c6bb9f50d04aabf688f84fe277ccdcbb2b
alperenozdnc <alperen@ozdinc.org>
2025-12-02 19:54:54 +0300

    feat(ezdocs): now `section_categorizer` also inserts a footer

--- src/ezdocs/opts/section_categorizer.c
+++ src/ezdocs/opts/section_categorizer.c
@@ -4,7 +4,7 @@
 
 #include "section_categorizer.h"
 
-ret_e _section_categorizer(void *ctx, CLI_IGNORE_TOK) {
+ret_e _print_section_header(void *ctx, CLI_IGNORE_TOK) {
     cli_s *cli = ctx;
 
     char *curr_tok = gettok_offset(cli, 0);
@@ -29,15 +29,41 @@ ret_e _section_categorizer(void *ctx, CLI_IGNORE_TOK) {
     return RET_NORMAL;
 }
 
-void _section_categorizer_add(cli_s *cli) {
-    static char *aliases[] = CLI_ALIASES(CLI_COMMON_OPT);
+ret_e _print_section_footer(void *ctx, CLI_IGNORE_TOK) {
+    cli_s *cli = ctx;
 
-    opt_s section_categorizer = {
-        .aliases = aliases,
-        .body = _section_categorizer,
+    char *curr_tok = gettok_offset(cli, 0);
+    int curr_tok_len = strlen(curr_tok);
+
+    if (cli->argc < 3)
+        return RET_NORMAL;
+
+    printf("\n");
+
+    for (int i = 0; i < curr_tok_len * 3; i++)
+        putchar('-');
+
+    printf("\n\n");
+
+    return RET_NORMAL;
+}
+
+void _section_categorizer_add(cli_s *cli) {
+    static char *aliases_header[] = CLI_ALIASES(CLI_COMMON_OPT);
+    static char *aliases_footer[] = CLI_ALIASES(CLI_POST_COMMON_OPT);
+
+    opt_s section_header = {
+        .aliases = aliases_header,
+        .body = _print_section_header,
         .ctx = cli,
-        .desc = "section header formatter",
     };
 
-    allocopt(cli, &section_categorizer);
+    opt_s section_footer = {
+        .aliases = aliases_footer,
+        .body = _print_section_footer,
+        .ctx = cli,
+    };
+
+    allocopt(cli, &section_header);
+    allocopt(cli, &section_footer);
 }
