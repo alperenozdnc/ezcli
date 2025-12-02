600144d50185eccc93e191f3aec7c61f85b29827
alperenozdnc <alperen@ozdinc.org>
2025-12-02 19:54:33 +0300

    refactor(internal): blacklist for special options

---
+++ src/ezcli/internal/blacklist.c
@@ -0,0 +1,8 @@
+#include <ezcli/opt.h>
+
+#include "blacklist.h"
+
+#include <stddef.h>
+
+char *BLACKLIST[] = {CLI_NONOPT, CLI_DEFAULT_OPT, CLI_COMMON_OPT,
+                     CLI_POST_COMMON_OPT, NULL};
