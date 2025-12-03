2a7464d13a27791d1a3da10ffd3fb56ab4184e3e
alperenozdnc <alperen@ozdinc.org>
2025-12-03 03:36:02 +0300

    refactor(ezdocs): ezdocs now skips description section

--- src/ezdocs/func/print_docfile.c
+++ src/ezdocs/func/print_docfile.c
@@ -1,19 +1,21 @@
 #include <ezcli.h>
 
+#include "../main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void print_docfile(char *docname) {
     const char *HOME = getenv("HOME");
-    char *dir = "/.local/ezdocs/";
 
-    size_t size = strlen(HOME) + strlen(dir) + strlen(docname) +
-                  strlen(".docs") + sizeof('\0');
+    size_t size = strlen(HOME) + strlen(LOCAL_EZCLI_DIR) + strlen(docname) +
+                  strlen(DOCS_FILE_EXT) + sizeof('\0');
 
     char *doc_file_path = malloc(size);
 
-    snprintf(doc_file_path, size, "%s%s%s.docs", HOME, dir, docname);
+    snprintf(doc_file_path, size, "%s%s%s%s", HOME, LOCAL_EZCLI_DIR, docname,
+             DOCS_FILE_EXT);
 
     FILE *doc_file = fopen(doc_file_path, "r");
 
@@ -27,13 +29,43 @@ void print_docfile(char *docname) {
         exit(EXIT_FAILURE);
     }
 
+    char buffer[1024] = "";
+    int i = 0;
+
+    bool is_description_over = false;
+    bool skip_after_delimiter = false;
+
     while (true) {
         char c = fgetc(doc_file);
 
         if (c == EOF)
             break;
 
-        putchar(c);
+        if (is_description_over && !skip_after_delimiter) {
+            putchar(c);
+
+            continue;
+        }
+
+        if (is_description_over) {
+            skip_after_delimiter = false;
+            continue;
+        }
+
+        buffer[i++] = c;
+
+        if (c != '\n')
+            continue;
+
+        buffer[i] = '\0';
+
+        if (strcmp(DESC_DELIMITER, buffer) == 0) {
+            is_description_over = true;
+            skip_after_delimiter = true;
+        } else {
+            buffer[0] = '\0';
+            i = 0;
+        }
     }
 
     fclose(doc_file);
--- src/ezdocs/main.h
+++ src/ezdocs/main.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define LOCAL_EZCLI_DIR "/.local/ezdocs/"
+#define DOCS_FILE_EXT ".docs"
+#define DESC_DELIMITER "DESCRIPTION_END\n"
+
 /*
  * template for a subject documentation option.
  */
