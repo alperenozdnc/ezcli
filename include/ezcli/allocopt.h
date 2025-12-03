20cd6b08781b6c7d0624befe14f89b1f1759ef81
alperenozdnc <alperen@ozdinc.org>
2025-12-04 00:10:12 +0300

    feat(ezcli): `allocopt()` now owns alls fields of the opt passed to it

--- include/ezcli/allocopt.h
+++ include/ezcli/allocopt.h
@@ -4,6 +4,11 @@
 #include <ezcli/opt.h>
 
 /*
- * allocates an `opt_s` struct in heap and adds it to `cli->opts`.
+ * allocates an `opt_s` struct and all its stack-allocated fields
+ * in heap and adds it to `cli->opts`.
+ *
+ * ezcli owns memory of all opts passed to `allocopt()`. your only
+ * responsibility is to keep the `ctx` and `body` fields alive, and run
+ * `freecli()` at the end of your program.
  */
 void allocopt(cli_s *cli, const opt_s *stack_opt);
--- src/ezcli/utils/allocopt.c
+++ src/ezcli/utils/allocopt.c
@@ -3,12 +3,35 @@
 #include <ezcli/cli.h>
 #include <ezcli/opt.h>
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void allocopt(cli_s *cli, const opt_s *opt_stack) {
     opt_s *opt = malloc(sizeof *opt);
-    *opt = *opt_stack;
+    size_t aliases_len = 0;
 
+    while (opt_stack->aliases[aliases_len])
+        aliases_len++;
+
+    opt->aliases = malloc(sizeof(char *) * (aliases_len + 1));
+
+    for (size_t i = 0; i < aliases_len; i++) {
+        opt->aliases[i] = strdup(opt_stack->aliases[i]);
+    }
+
+    opt->aliases[aliases_len] = NULL;
+
+    opt->body = opt_stack->body;
+    opt->ctx = opt_stack->ctx;
+
+    if (opt_stack->desc) {
+        opt->desc = strdup(opt_stack->desc);
+    } else {
+        opt->desc = NULL;
+    }
+
+    opt->want_input = opt_stack->want_input;
     opt->allocated = true;
 
     addopt(cli, opt);
