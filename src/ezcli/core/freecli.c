7ec593e1fedda756736afa27ba294fc7457cb9c9
alperenozdnc <alperen@ozdinc.org>
2025-12-04 00:10:31 +0300

    feat(core): free all fields of all heap-allocated opts

--- src/ezcli/core/freecli.c
+++ src/ezcli/core/freecli.c
@@ -5,9 +5,26 @@
 
 void freecli(cli_s *cli) {
     for (size_t i = 0; i < cli->opts_len; i++) {
-        if (cli->opts[i]->allocated) {
-            free(cli->opts[i]);
+        opt_s *opt = cli->opts[i];
+
+        if (!opt->allocated) {
+            continue;
         }
+
+        int j = 0;
+
+        while (opt->aliases[j]) {
+            free(opt->aliases[j]);
+
+            j++;
+        }
+
+        free(opt->aliases);
+
+        if (opt->desc)
+            free(opt->desc);
+
+        free(cli->opts[i]);
     }
 
     free(cli->opts);
