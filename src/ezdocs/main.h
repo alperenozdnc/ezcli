652e981ecb0e147e4d404efbd8d99b988d38c427
alperenozdnc <alperen@ozdinc.org>
2025-12-04 20:11:33 +0300

    chore(ezdocs): remove unused `DOC_OPT` macro

--- src/ezdocs/main.h
+++ src/ezdocs/main.h
@@ -3,12 +3,3 @@
 #define LOCAL_EZCLI_DIR "/.local/ezdocs/"
 #define DOCS_FILE_EXT ".docs"
 #define DESC_DELIMITER "DESCRIPTION_END\n"
-
-/*
- * template for a subject documentation option.
- */
-#define DOC_OPT(alias, descr)                                                  \
-    (opt_s) {                                                                  \
-        .aliases = CLI_ALIASES(alias), .desc = descr, .body = doc_body,        \
-        .ctx = &cli,                                                           \
-    }
