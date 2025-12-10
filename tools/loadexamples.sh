ee77566bf509099292216fbd25e45db7e332a966
alperenozdnc <alperen@ozdinc.org>
2025-12-10 15:59:43 +0300

    chore: `tools` -> `scripts`

---
+++ tools/loadexamples.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+# run source ./tools/loadaliases.sh from project root
+# to get aliases for the example programs
+
+alias calc="./build/examples/calc"
+alias human="./build/examples/human"
+alias program="./build/examples/program"
+alias recursor="./build/examples/recursor"
+alias minilang="./build/examples/minilang"
