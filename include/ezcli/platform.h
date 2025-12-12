7050827eac3af72c54567b4b5b586e477397bf1f
alperenozdnc <alperen@ozdinc.org>
2025-12-12 17:40:12 +0300

    chore: add comments for platform header

--- include/ezcli/platform.h
+++ include/ezcli/platform.h
@@ -13,17 +13,24 @@ int c_printf(const char *restrict format, ...);
 int c_puts(const char *str);
 
 /*
- * exit handler. for embedded systems, this usually halts or resets.
+ * basic exiting function.
+ * for embedded systems, this isn't defined. this usually halts or resets.
  * for pc, this maps out to libc exit().
  */
 void c_exit(int status);
 
 /*
- * for basic string funcrtionss
+ * basic string functions. for embedded systems, ezcli provides standalone
+ * implementations. for pc, these map out to their libc counterparts.
  */
 size_t c_strlen(const char *str);
 int c_strcmp(const char *a, const char *b);
 
+/*
+ * memory allocation functions. for embedded systems, these don't have
+ * definitions, and don't run anyways. for pc, these map out to their libc
+ * counterparts.
+ */
 void *c_realloc(void *ptr, size_t n);
 void *c_malloc(size_t n);
 void c_free(void *ptr);
