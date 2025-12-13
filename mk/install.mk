319bb7eefe44aae3121466940957d36691e828a4
alperenozdnc <alperen@ozdinc.org>
2025-12-13 21:41:25 +0300

    build: make compilation & installation perfectly reverse each other i'm meaning that doing 'make && make install && make install-docs' can be perfectly reverted filesystem-wise with 'make clean && make uninstall && make uninstall-docs'

--- mk/install.mk
+++ mk/install.mk
@@ -1,27 +1,35 @@
 #============== installation/uninstallation ==============
 
 EZCLI_INCLUDE_DIR= $(INCLUDE_DIR)/ezcli
-EZCLI_UMBRELLA_HEADER= $(INCLUDE_DIR)/ezcli.h
+
+EZCLI_UMBRELLA_HEADER_NAME = ezcli.h
+
+_EZCLI_UMBRELLA_HEADER = $(INCLUDE_DIR)/$(EZCLI_UMBRELLA_HEADER_NAME)
+EZCLI_UMBRELLA_HEADER = $(USR_INCLUDE_DIR)/$(EZCLI_UMBRELLA_HEADER_NAME)
+
 EZCLI_COMPLETIONS_CMD = autocompletions
 
 .PHONY: clean install uninstall install-docs uninstall-docs
 
 install:
 	sudo mkdir -p $(USR_INCLUDE_DIR)/$(EZCLI_DIR)
-	sudo cp $(EZCLI_UMBRELLA_HEADER) $(USR_INCLUDE_DIR)/
+	sudo cp $(_EZCLI_UMBRELLA_HEADER) $(USR_INCLUDE_DIR)/
 	sudo cp $(EZCLI_INCLUDE_DIR)/*.h $(USR_INCLUDE_DIR)/ezcli/
 	sudo cp $(EZCLI_LIB_PATH) $(USR_LIB_DIR)/
 
 uninstall:
+	rm -rf $(COMPLETION_TEMPLATES_DIR)
+
 	sudo rm -f $(EZCLI_UMBRELLA_HEADER)
 	sudo rm -rf $(USR_INCLUDE_DIR)/$(EZCLI_DIR)
 	sudo rm -f $(USR_LIB_DIR)/$(EZCLI_LIB_NAME)
 
 install-docs: $(EZDOCS_BIN)
 	sudo cp $(EZDOCS_BIN) $(USR_BIN_DIR)/$(EZDOCS_NAME)
-	sudo -E $(INSTALL_BIN_DIR)/$(EZDOCS_NAME) $(EZCLI_COMPLETIONS_CMD)>/dev/null
+	sudo -E $(EZDOCS_NAME) $(EZCLI_COMPLETIONS_CMD)>/dev/null
 
 uninstall-docs:
+	rm -rf $(EZDOCS_CONTENT_DIR)
 	sudo rm -f $(USR_BIN_DIR)/$(EZDOCS_NAME)
 	sudo rm -f $(EZDOCS_COMPLETIONS_BASH)
 	sudo rm -f $(EZDOCS_COMPLETIONS_ZSH)
