94b2a6ed0672f7646bd456f155a264e443f75778
alperenozdnc <alperen@ozdinc.org>
2025-12-19 23:38:20 +0300

    refactor(tools): improve the `versioner` tool

--- tools/versioner.c
+++ tools/versioner.c
@@ -1,107 +1,228 @@
 #include <ezcli.h>
+
+#include "internal/join_str.h"
+
+#include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *join_str(char *str1, char *str2) {
-    size_t size = strlen(str1) * strlen(str2) + sizeof('\0');
+#define TMP_FILE_NAME "/tmp/versioner_commits.txt"
+#define OUTPUT_FILE_NAME "version.txt"
 
-    char *str3 = malloc(size);
+#define clean_tmp_file() system("rm -rf " TMP_FILE_NAME)
+#define build_str(...) _build_str(NULL, __VA_ARGS__, NULL)
+#define clear_screen() system("clear")
 
-    snprintf(str3, size, "%s%s", str1, str2);
+#define BUF_SIZE 256
 
-    return str3;
+/*
+ * small helper for concatenating a variable amount of strings.
+ */
+char *_build_str(void *start, ...) {
+    char *ret_str = NULL;
+    char *tmp = NULL;
+
+    va_list args;
+    va_start(args, start);
+
+    while (true) {
+        char *str = va_arg(args, char *);
+
+        if (!str)
+            break;
+
+        if (!ret_str) {
+            ret_str = strdup(str);
+
+            continue;
+        }
+
+        tmp = join_str(ret_str, str);
+        free(ret_str);
+
+        ret_str = strdup(tmp);
+        free(tmp);
+    }
+
+    va_end(args);
+
+    (void)start;
+
+    return ret_str;
 }
 
-void get_input(char *buffer) {
-    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+/*
+ * gets input from user and writes into `buf`.
+ */
+void get_input(char *buf) {
+    if (fgets(buf, BUF_SIZE, stdin) == NULL) {
         cliprint(CLI_ERROR, "versioner > ", "error reading input.");
         exit(EXIT_FAILURE);
     }
 
     // fgets includes the \n for when return is pressed
-    size_t trim_len = strcspn(buffer, "\n");
-    buffer[trim_len] = '\0';
+    size_t trim_len = strcspn(buf, "\n");
+    buf[trim_len] = '\0';
 }
 
-void write_commit(FILE *fptr, char *commit_name, char *version) {
+/*
+ * writes commit and its corresponding version to the output file.
+ */
+void commit_write(FILE *fptr, char *commit_name, char *version) {
     fprintf(fptr, "%s -> %s\n\n", commit_name, version);
 }
 
-ret_e body_start(CLI_IGNORE_ARGS) {
+/*
+ * gets and lists available tags to reference from.
+ */
+void list_tags() {
     printf("all available tags: \n\n");
     system("git tag");
     printf("\n");
+}
+
+/*
+ * gets the reference tag that the user wants to reference commits against.
+ */
+char *get_ref_tag() {
+    static char buf[BUF_SIZE];
+
+    list_tags();
 
     printf("versioner > which tag do you want to compare? ");
+    get_input(buf);
 
-    char buffer[256];
+    char *cmd = build_str("git log", " ", buf,
+                          "...HEAD --oneline --reverse --pretty=format:'%s'",
+                          " ", ">>", " ", TMP_FILE_NAME);
 
-    get_input(buffer);
-
-    char *lhs = join_str("git log ", buffer);
-    char *rhs = "...HEAD --oneline --reverse --pretty=format:'%s'";
-    char *git_cmd = join_str(lhs, rhs);
-    char *cmd = join_str(git_cmd, ">> /tmp/versioner_commits.txt");
-
-    system("rm -rf /tmp/versioner_commits.txt");
+    clean_tmp_file();
     system(cmd);
 
-    free(lhs);
-    free(git_cmd);
     free(cmd);
 
-    char commit[256];
-    FILE *commits_fptr = fopen("/tmp/versioner_commits.txt", "r");
-    FILE *version_file_fptr = fopen("./version.txt", "w");
+    return buf;
+}
 
-    char *initial_version = strdup(buffer);
-    char *curr_version = strdup(buffer);
-    char tmp[256];
+/*
+ * prints the current version and the next commit in the iteration.
+ */
+void print_curr_info(char *vers_curr, char *commit) {
+    printf("versioner > current version is %s\n\n", vers_curr);
+    printf("commit > %s\n\n", commit);
+    printf("new version or leave empty or 'abort': ");
+}
 
-    int commit_count = 0;
-    int bump_count = 0;
+/*
+ * prints the new version, amount of commits and bumps, and gives information
+ * about the output file for kind of a closing 'ritual'.
+ */
+void print_final_info(char *vers_initial, char *vers_curr, size_t commit_cnt,
+                      size_t bump_cnt) {
+    printf("versioner > %s -> %s, %ld commits, %ld bumps.\n", vers_initial,
+           vers_curr, commit_cnt, bump_cnt);
 
-    while (fgets(commit, sizeof(commit), commits_fptr)) {
-        system("clear");
+    printf("versioner > all commits and their corresponding versions have been "
+           "written to " OUTPUT_FILE_NAME " in your current directory.\n");
+}
 
-        printf("versioner > current version is %s\n\n", curr_version);
-        printf("commit > %s\n\n", commit);
+/*
+ * the cleanup ritual.
+ */
+void cleanup(FILE *file_commits, FILE *file_output, char *vers_curr,
+             char *vers_initial) {
+    fclose(file_commits);
+    fclose(file_output);
 
-        printf("new version or leave empty: ");
+    clear_screen();
+    clean_tmp_file();
+
+    free(vers_curr);
+    free(vers_initial);
+}
+
+/*
+ * this directs the user to type 'abort' instead of just `ctrl-c` to avoid
+ * undeleted artifacts.
+ */
+void direct_to_abort(__attribute((unused)) int code) {
+    clear_screen();
+
+    printf("you should write 'abort' to make sure versioner can correctly "
+           "clean up. press enter to continue. \n\n");
+}
+
+bool validate_file(FILE *fptr, char *path) {
+    if (!fptr) {
+        cliprint(CLI_ERROR, "versioner > ", "couldn't read file '%s'.", path);
+
+        return false;
+    }
+
+    return true;
+}
+
+/*
+ * the main option that compiles querying, iterating, and cleanup together.
+ */
+ret_e body_start(CLI_IGNORE_ARGS) {
+    signal(SIGINT, direct_to_abort);
+
+    char commit[BUF_SIZE];
+    char tmp[BUF_SIZE];
+
+    char *ref_tag = get_ref_tag();
+
+    FILE *file_commits = fopen(TMP_FILE_NAME, "r");
+    FILE *file_output = fopen(OUTPUT_FILE_NAME, "w");
+
+    if (!validate_file(file_commits, TMP_FILE_NAME) ||
+        !validate_file(file_output, OUTPUT_FILE_NAME)) {
+        return RET_FAIL;
+    }
+
+    char *vers_initial = strdup(ref_tag);
+    char *vers_curr = strdup(ref_tag);
+
+    size_t commit_cnt = 0;
+    size_t bump_cnt = 0;
+
+    while (fgets(commit, BUF_SIZE, file_commits)) {
+        clear_screen();
+
+        print_curr_info(vers_curr, commit);
 
         get_input(tmp);
 
+        if (strcmp(tmp, "abort") == 0) {
+            cleanup(file_commits, file_output, vers_curr, vers_initial);
+
+            printf("aborting.\n");
+
+            return RET_FAIL;
+        }
+
         if (strlen(tmp) == 0) {
-            write_commit(version_file_fptr, commit, curr_version);
-            commit_count++;
+            commit_write(file_output, commit, vers_curr);
+            commit_cnt++;
 
             continue;
         }
 
-        free(curr_version);
-        curr_version = strdup(tmp);
+        free(vers_curr);
+        vers_curr = strdup(tmp);
 
-        write_commit(version_file_fptr, commit, curr_version);
-        bump_count++;
+        commit_write(file_output, commit, vers_curr);
 
-        commit_count++;
+        commit_cnt++;
+        bump_cnt++;
     }
 
-    system("clear");
-    system("rm -rf /tmp/versioner_commits.txt");
+    print_final_info(vers_initial, vers_curr, commit_cnt, bump_cnt);
 
-    printf("versioner > %s -> %s, %d commits, %d bumps.\n", initial_version,
-           curr_version, commit_count, bump_count);
-    printf("versioner > all commits and their corresponding versions have been "
-           "written to "
-           "'version.txt' in your current directory.\n");
-
-    free(curr_version);
-    free(initial_version);
-
-    fclose(commits_fptr);
-    fclose(version_file_fptr);
+    cleanup(file_commits, file_output, vers_curr, vers_initial);
 
     return RET_NORMAL;
 }
